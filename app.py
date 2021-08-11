e88e5f463ba5b2c78de758ce375f4e9e9b88cafd
Eric Ruan <eric.ruan.130@gmail.com>
2021-08-11 18:22:41 -0400

    Used the new dataset

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-DATA_URL = 'https://raw.githubusercontent.com/STRIDES-Codes/Uncovering-the-Nutritional-Landscape-of-Food/main/nutrient_food_unit_value.tsv'
+
+DATA_URL = 'https://raw.githubusercontent.com/STRIDES-Codes/Uncovering-the-Nutritional-Landscape-of-Food/main/nutrient_foodname_DRI.csv'
 
 st.title('Uncovering the Nutritional Landscape of Food')
 
 
 @st.cache
 def load_data():
-    df = pd.read_csv(DATA_URL, sep='\t')
+    df = pd.read_csv(DATA_URL, sep=',')
     df = df.astype({'unit': 'string', 'food': 'string',
                     'nutrient_value': 'float64'})
-    print(df.dtypes)
     return df
 
 
@@ -30,7 +30,7 @@ with st.container():
     col1, col2 = st.columns(2)
 
     with col1:
-        unique_nutrients = food['nutrient'].unique().tolist()
+        unique_nutrients = food['DRI_name'].unique().tolist()
         unique_nutrients = [
             nutrient for nutrient in unique_nutrients if 'Vitamin' in nutrient]
         vitamins_keys = st.multiselect(
@@ -39,7 +39,7 @@ with st.container():
         )
 
     with col2:
-        unique_minerals = food['nutrient'].unique().tolist()
+        unique_minerals = food['DRI_name'].unique().tolist()
         unique_minerals = [
             nutrient for nutrient in unique_minerals if 'Vitamin' not in nutrient and ':' not in nutrient and ', ' in nutrient]
         mineral_keys = st.multiselect(
@@ -48,18 +48,31 @@ with st.container():
         )
 
     total_checkbox = st.checkbox('Show total only')
+    gender_checkbox = st.checkbox('Male')
 
-    def create_barcharts(df, keys=[]):
+    def create_barcharts(df, keys=[], male=True):
         keys = sorted(keys, reverse=True)
-        df = df.loc[df['nutrient'].isin(keys)].sort_values(
-            by=['nutrient'], ascending=True)
+        df = df.loc[df['DRI_name'].isin(keys)].sort_values(
+            by=['DRI_name'], ascending=True)
         data = {}
 
         for row in df.itertuples(index=False):
             data.setdefault(row[2], {})
+
+            dri = (row[3] * row[6] /
+                   row[7]) if male else (row[3] * row[6] / row[8])
+
+            if row[4] in data[row[2]]:
+                if 'added' in row[0]:
+                    data[row[2]].update({row[4]: data[row[2]][row[4]] + dri})
             data[row[2]].setdefault(
-                row[0], row[3]
+                row[4], dri
             )
+
+        # row[2] = Food name
+        # row[3] = nutrient_value
+        # row[6] = Conversion factor
+        # row[4] = DRI name (simplified nutrient name)
 
         major_category = []
         for k in keys:
@@ -86,7 +99,7 @@ with st.container():
                         data[food_name][major_category[i]] = 0
                         array.append(0)
             fig.add_bar(y=y, x=array, orientation='h', name=food_name,
-                        text=array, textposition='auto')
+                        text=array, textposition='auto', texttemplate='%{text:.2f}')
 
         fig.update_layout(
             barmode='relative',
@@ -97,18 +110,34 @@ with st.container():
 
         return st.plotly_chart(fig)
 
-    def create_barchartsStacked(df, keys=[]):
+    def create_barchartsStacked(df, keys=[], male=True):
         keys = sorted(keys, reverse=False)
-        df = df.loc[df['nutrient'].isin(keys)].sort_values(
-            by=['nutrient'], ascending=True)
+        df = df.loc[df['DRI_name'].isin(keys)].sort_values(
+            by=['DRI_name'], ascending=True)
 
         data = {}
 
         for row in df.itertuples(index=False):
             data.setdefault(row[2], [])
-            data[row[2]].append(
-                (row[0], row[3])
-            )
+
+            found = False
+
+            for x in data[row[2]]:
+                if row[4] in x:
+                    if 'added' in row[0]:
+                        x[1] += (row[3] * row[6] /
+                                 row[7]) if male else (row[3] * row[6] / row[8])
+                    else:
+                        found = True
+                        break
+
+            if not found:
+                data[row[2]].append(
+                    [row[4], (row[3] * row[6] /
+                              row[7]) if male else (row[3] * row[6] / row[8])]
+                )
+
+        print(data)
 
         bar = []
         for key in data:
@@ -126,22 +155,26 @@ with st.container():
             text='Micronutrients'))
         fig.update_yaxes(title=dict(
             text='Percentage of daily intake in decimal'), rangemode='tozero')
-
         return st.plotly_chart(fig)
 
     if options != []:
         selected = food.loc[food['food'].isin(options)]
+        st.write(selected)
         if vitamins_keys != []:
             if total_checkbox:
-                create_barchartsStacked(selected, keys=vitamins_keys)
+                create_barchartsStacked(
+                    selected, keys=vitamins_keys, male=gender_checkbox)
             else:
-                create_barcharts(selected, keys=vitamins_keys)
+                create_barcharts(selected, keys=vitamins_keys,
+                                 male=gender_checkbox)
 
         if mineral_keys != []:
             if total_checkbox:
-                create_barchartsStacked(selected, keys=mineral_keys)
+                create_barchartsStacked(
+                    selected, keys=mineral_keys, male=gender_checkbox)
             else:
-                create_barcharts(selected, keys=mineral_keys)
+                create_barcharts(selected, keys=mineral_keys,
+                                 male=gender_checkbox)
 
         if mineral_keys == [] and vitamins_keys == []:
             st.write('Please select micronutrient(s)')
